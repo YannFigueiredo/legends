0a07b1a45521f84289bd20268344c33c2bf55663
Yann Figueiredo <yann.fabricio@hotmail.com>
2021-10-25 23:12:33 -0300

    partially finished the slides and comments sections

--- js/main.js
+++ js/main.js
@@ -4,4 +4,16 @@
         btnMenu: '.btn-menu',
         widthEnabled: 1025
     });
+
+    var slides = new Slide({
+        imagem: '.photos-slides figure',
+        btnPrev: '.slides .prev',
+        btnNext: '.slides .next'
+    })
+
+    var comments = new Slide({
+        imagem: '.comments figure',
+        btnPrev: '.comments .prev',
+        btnNext: '.comments .next'
+    })
 })();
